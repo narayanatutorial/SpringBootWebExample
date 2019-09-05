2151f6bcdcbc4292cddaed3acad6b3a9a63d86e4
narayanatutorial <narayanaswamy.ragi@gmail.com>
2019-09-05 19:13:40 +0530

    initial commit

---
+++ src/main/java/com/narayanatutorial/SpringBootWebExample/SpringBootWebExampleApplication.java
@@ -0,0 +1,13 @@
+package com.narayanatutorial.SpringBootWebExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebExampleApplication.class, args);
+	}
+
+}
